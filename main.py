81feebd4975cab1c2b6ba76a22f0b59414c53c2d
Зацепин Серафим Романович <zatsepin.s.r@mac205-06.ouiit.local>
2025-03-12 12:50:58 +0300

    another commit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settinfrom
+from config import MY_FIO
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f"Hello, {MY_FIO}")  # Press ⌘F8 to toggle the breakpoint.
